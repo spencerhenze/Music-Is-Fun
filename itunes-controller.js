1166776b7deebc6f207bca9348baa51be45ca003
Spencer Henze <spencerhenze@gmail.com>
2017-08-02 13:57:31 -0600

    Created App

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,17 +1,64 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(); //after get music by artist returns what are you doing with the objects?
+    itunesService.getMusicByArtist(artist).then(drawSongs); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
+  // var isPlaying = false;
+
+  // function setFlag(){
+  //   if(isPlaying == true){
+  //     isPlaying = false
+  //     return isPlaying;
+  //   }
+  //   isPlaying = true
+  // }
+  function clearSongs(){
+    var nothing = '';
+    document.getElementById('song-titles').innerHTML = nothing;
+  }
+
+  function drawSongs(songs) {
+    var counter = 0
+
+    if(counter != 0){
+      clearSongs();
+    }
+
+    var template = '';
+
+    songs.forEach(function (song) {
+      template += `
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+          <div class="thumbnail card-wrapper">
+              <h3 class="center-text">${song.artist}</h3>
+              <img class="album-artwork" src="${song.albumArt}" alt="art">
+              <div class="info">
+                  <h5><strong>Title:</strong> ${song.title}</h5>
+                  <h5><strong>Album:</strong> ${song.collection}</h5>
+                  <h5><strong>Price:</strong> ${song.price}</h5>
+                  <audio controls onclick="setFlag()">
+                      <source src="${song.preview}" type="audio/mpeg">
+                  </audio>
+              </div>
+          </div>
+      </div>
+      `
+    });
+
+
+    document.getElementById('song-tiles').innerHTML = template;
+
+    counter++;
+
+  }
 
 
 
 
 
-  
 }
--- itunes-service.js
+++ itunes-service.js
@@ -1,30 +1,42 @@
-function ItunesService(){
+function ItunesService() {
 
-    this.getMusicByArtist = function(artist) {
+  this.getMusicByArtist = function (artist) {
 
-      //allows requests to localhost: 8080 otherwise blocked by itunes
-      var url = '//bcw-getter.herokuapp.com/?url=';
-      var url2 = 'https://itunes.apple.com/search?term=' + artist;
-      var apiUrl = url + encodeURIComponent(url2);
-      
-      //changes the button to loading while songs load
-      $('#get-music-button').text('LOADING....');
-      
-      //modifies the objects to reduce the excess data
-      return $.getJSON(apiUrl).then(function(response){
-        var songList = response.results.map(function (song) {
-                  return {
-                      title: song.trackName,
-                      albumArt: song.artworkUrl60,
-                      artist: song.artistName,
-                      collection: song.collectionName,
-                      price: song.collectionPrice,
-                      preview: song.previewUrl
-                    };
-                })
-        //changes button back to GET MUSIC once songs are loaded
-        $('#get-music-button').text('GET MUSIC');
-        return songList;
+    //allows requests to localhost: 8080 otherwise blocked by itunes
+    var url = '//bcw-getter.herokuapp.com/?url=';
+    var url2 = 'https://itunes.apple.com/search?term=' + artist;
+    var apiUrl = url + encodeURIComponent(url2);
+
+    //changes the button to loading while songs load
+    $('#get-music-button').text('LOADING....');
+
+    //modifies the objects to reduce the excess data
+    return $.getJSON(apiUrl).then(function (response) {
+
+      //make a new array that will hold the items that are not videos
+      var filteredList = [];
+      //loop through the API results and push only the songs to the new array
+      response.results.forEach(function(item) {
+          if(item.kind == 'song'){
+            filteredList.push(item);
+          }
+        });
+
+      //map the filtered list to the output variable
+      var songList = filteredList.map(function (song) {
+
+          return {
+            title: song.trackName,
+            albumArt: song.artworkUrl60,
+            artist: song.artistName,
+            collection: song.collectionName,
+            price: song.collectionPrice,
+            preview: song.previewUrl
+          };
       })
-    }
+      //changes button back to GET MUSIC once songs are loaded
+      $('#get-music-button').text('GET MUSIC');
+      return songList;
+    })
+  }
 }
